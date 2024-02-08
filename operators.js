eeb09fe5a97fef3204ff7c1ffa98727812008794
kadir-zeren <k4dzeren@gmail.com>
2024-02-09 00:10:38 +0300

    <> operators ==

--- operators.js
+++ operators.js
@@ -117,3 +117,42 @@ console.log(artir);
 let newSayi = ++artir;
 console.log(newSayi);
 
+let nums1 = 14;
+let nums2 = 17;
+
+nums2 += nums1;
+console.log(nums2);
+
+const num16 = 27;
+
+console.log(num16 == 27);
+console.log(num16 == "27");
+
+console.log(num16 === 27);
+console.log(num16 === "27");
+
+// const user1 = prompt(`lutfen sayi giriniz`);
+// const user2 = prompt(`lutfen sayi giriniz`);
+
+// console.log(user1);
+// console.log(user2);
+// const isSame = user1 == user2;
+// console.log(isSame);
+
+console.log(true === 1);
+console.log(null === undefined);
+
+console.log(15 != `15`);
+console.log(15 !== `15`);
+
+console.log(15 > 5);
+console.log(15 < 5);
+
+console.log(`a` > `A`);
+
+console.log(15 >= 15);
+console.log(15 <= 15);
+console.log(15 >= 16);
+
+console.log(15 >= `15`);
+console.log(`14` <= `15`);
